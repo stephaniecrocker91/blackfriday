e995cb1eb7341c82ae044b2aaf1a81f25f41d660
Stephie <stephaniecrocker91@gmail.com>
2023-08-30 14:53:12 +0000

    add fake navbar with logo on top

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import styles from '../styles/Landing.module.css'
 import herovideo from '../videos/herovideo.mp4'
-
+import logo from '../images/logo.png'
 
 
 function Landing() {
     return (
         <>
-        <p>Lash Out on Savings: Up to <strong>70% Off</strong> Beauty Products this Black Friday!</p>
+        <p>Unlock <strong>VIP Access</strong>  to Exclusive Deals Before Anyone Else!</p>
+        <img src={logo} alt="Girl in a jacket" className={styles.Logo}  />      
         <div className={styles.Hero}>
             <div className={styles.overlay}></div>
             <video src={herovideo} autoPlay loop muted />
             <div className={styles.Content}>
                 <h2>Up to <strong>70% off</strong></h2>
-                    <h3>London <strong>Lash</strong> </h3>
                     <h1>Black Friday <strong>Sales</strong></h1>            
                 <h2>Are almost here...</h2>
             </div>
