1747c69dcf14738f309c31fc725498eac010e96b
Stephie <stephaniecrocker91@gmail.com>
2023-08-31 20:51:07 +0000

    updateColor Landing page useEffect

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,38 @@
-import { useRef, React } from 'react';
+import { React, useState, useEffect } from 'react';
 import styles from '../styles/Landing.module.css'
 import s from '../styles/Banner.module.css'
 import hero from '../images/hero.png'
 
+
+let defaultColor = "white"
+let num = 0
+
 function Landing() {
+
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(()=> {
+            /* Created every second */
+            updateColor(defaultColor);
+        }, 1000);
+        return () => clearInterval(intervalId)
+    }, [])
+
+    function updateColor(color) {
+        console.log("Hello, World!")
+        num+= 1
+        console.log(num)
+        if(color = "white"){
+            let defaultColor= "black";
+        } else {
+            let defaultColor="white";
+        }
+        console.log(defaultColor);
+    };
+
+
+
     return (
         <>  
         <div className={styles.Hero}>
@@ -11,14 +40,12 @@ function Landing() {
             <img src={hero} alt='Hero with lashes' />
             <div className={styles.Content}>
                 <div>
-                <h1>Black Friday <strong>Sales</strong></h1>
+                <h1 style={{ color:[defaultColor] }}>Black Friday <strong>Sales</strong></h1>
                 <h2>Are almost here...</h2>
                 </div>            
             </div>
         </div>
-
             <p className={s.Banner} >Lash Out on Savings: Up to <strong>70% Off</strong> Beauty Products this Black Friday!</p>
-
         </>
     );
 }
