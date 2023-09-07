d2cafa69f6de5d006dc6e1fc63999a391d5ad34b
Stephie <stephaniecrocker91@gmail.com>
2023-09-07 13:43:33 +0000

    Two carousel, one not working overlay, other overlay works but has third glitch slide

--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -4,15 +4,50 @@ import Hero2 from '../images/herocard2.png';
 import styles from '../styles/Landing.module.css';
 
 var heroData = [
-    {id: 1,
-    image: Hero,
+    {
+        id: 1,
+        image: Hero
 
-    },
-    {id: 2,
-    image: Hero2,
-}
+    }, {
+        id: 2,
+        image: Hero2
+    }
 ]
 
+function CarouselComponent() {
+    return (
+        <section id={
+                styles.home
+            }
+            className={
+                styles.HeroBlock
+        }>
+            <div className={
+                styles.carouselContainer
+            }>
+                <Carousel>
+                    <Carousel.Item interval={3000}>
+                        <img className="d-block w-100"
+                            src={Hero}
+                            alt="First slide"/>
+                    </Carousel.Item>
+                    <Carousel.Item interval={3000}>
+                        <img className="d-block w-100"
+                            src={Hero2}
+                            alt="Second slide"/>
+                    </Carousel.Item>
+                </Carousel>
+                <div className={
+                    styles.overlay
+                }></div>
+            </div>
+        </section>
+    );
+}
+
+export default CarouselComponent;
+
+{ /* Pverlay works but has a third faulty slide
 function CarouselComponent() {
     return (
         <section id ={styles.home}className={
@@ -30,8 +65,10 @@ function CarouselComponent() {
             }></div>
             </Carousel>
 
-            {/* Add the overlay class here */} </section>
+         </section>
     );
 }
 
 export default CarouselComponent;
+*/
+}
